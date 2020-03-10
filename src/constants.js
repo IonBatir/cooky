138ec44344c3f3615cde66dba29dac911a770591
Ion Batîr <ionbatir@gmail.com>
2020-03-10 21:32:38 +0200

    Add main constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const APP_NAME = 'Cooky';
+
+export const AUTH_STACK = 'AUTH';
+export const LOGIN_SCREEN = 'Login';
+export const REGISTER_SCREEN = 'Register';
+export const FORGOT_PASSWORD_SCREEN = 'Forgot Password';
+
+export const APP_STACK = 'APP';
+export const DASHBOARD_SCREEN = 'Dashboard';
