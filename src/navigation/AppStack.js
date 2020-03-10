3513ec91b7c5adce4340d47ce62e336ecc82aef1
Ion Batîr <ionbatir@gmail.com>
2020-03-10 21:34:39 +0200

    Add AppStack

---
+++ src/navigation/AppStack.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Dashboard } from '../screens';
+import { DASHBOARD_SCREEN } from '../constants';
+
+const AppStack = createStackNavigator();
+
+export default function() {
+  return (
+    <AppStack.Navigator>
+      <AppStack.Screen name={DASHBOARD_SCREEN} component={Dashboard} />
+    </AppStack.Navigator>
+  );
+}
