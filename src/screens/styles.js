456bd1f6d5a3755e13d3bb628726431b6f194471
Ion Batîr <ionbatir@gmail.com>
2020-03-29 18:20:01 +0300

    Add common styles

---
+++ src/screens/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+import { SPACING, FONT_FAMILY, FONT_SIZE } from '../theme';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: SPACING.MEDIUM,
+  },
+  centerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontFamily: FONT_FAMILY.BOLD,
+    fontSize: FONT_SIZE.EXTRA_EXTRA_LARGE,
+    color: '#1D2226',
+  },
+  subTitle: {
+    fontFamily: FONT_FAMILY.REGULAR,
+    fontSize: FONT_SIZE.LARGE,
+    color: '#1D2226',
+    opacity: 0.6,
+    marginBottom: SPACING.EXTRA_LARGE,
+  },
+});
