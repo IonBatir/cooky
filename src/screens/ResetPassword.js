c727b59e1b7511c83d32131a426e901901b79515
Ion Batîr <ionbatir@gmail.com>
2020-03-29 18:37:21 +0300

    Update ResetPassword screen

--- src/screens/ResetPassword.js
+++ src/screens/ResetPassword.js
@@ -1,18 +1,83 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Alert, StyleSheet } from 'react-native';
+import { resetPassword } from '../api/auth';
+import { TextField, Spinner, ErrorAlert } from '../components';
+import { SPACING, FONT_FAMILY, FONT_SIZE, COLOR } from '../theme';
+import { LOGIN_SCREEN } from '../constants';
+import commonStyles from './styles';
 
-export default function ResetPassword() {
-  return (
-    <View style={styles.container}>
-      <Text>Reset Password</Text>
+export default function ResetPassword({ navigation }) {
+  const [email, setEmail] = useState({ value: '', error: null });
+  const [loading, setLoading] = useState(false);
+
+  const handleResetPassword = () => {
+    if (email.value.length === 0) {
+      setEmail(state => ({ ...state, error: 'Please fill out this field' }));
+      return;
+    }
+
+    setLoading(true);
+    resetPassword(email.value)
+      .then(() => {
+        setLoading(false);
+        Alert.alert(
+          'Recover Password',
+          'We have sent you an email with instructions to reset your password.',
+          [{ text: 'OK', onPress: () => navigation.navigate(LOGIN_SCREEN) }],
+        );
+      })
+      .catch(error => {
+        setLoading(false);
+        const { userInfo } = error;
+        if (userInfo.code.includes('email')) {
+          setEmail(state => ({ ...state, error: userInfo.message }));
+          return;
+        }
+        ErrorAlert(userInfo.message);
+      });
+  };
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <View style={commonStyles.container}>
+      <Text style={commonStyles.title}>Forgot Password?</Text>
+      <Text style={commonStyles.subTitle}>
+        Please enter email to your account.
+      </Text>
+      <TextField
+        onChangeText={text => setEmail(state => ({ ...state, value: text }))}
+        onFocus={() => setEmail(state => ({ ...state, error: null }))}
+        onSubmitEditing={handleResetPassword}
+        value={email.value}
+        error={email.error}
+        placeholder="Email Address"
+        returnKeyType="go"
+        autoCompleteType="email"
+        keyboardType="email-address"
+        textContentType="emailAddress"
+        autoCapitalize="none"
+      />
+      <TouchableOpacity
+        style={styles.recoverButton}
+        onPress={handleResetPassword}>
+        <Text style={styles.recoverButtonText}>RESET PASSWORD</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  recoverButton: {
+    height: 52,
     justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: COLOR.PRIMARY,
+    marginVertical: SPACING.LARGE,
+  },
+  recoverButtonText: {
+    fontFamily: FONT_FAMILY.BOLD,
+    fontSize: FONT_SIZE.MEDIUM,
+    color: COLOR.WHITE,
+    textAlign: 'center',
   },
 });
