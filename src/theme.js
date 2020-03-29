68e2384e7d5c1254ce715d9e798c68406c2de715
Ion Batîr <ionbatir@gmail.com>
2020-03-29 13:31:15 +0300

    Add theme constants

---
+++ src/theme.js
@@ -0,0 +1,37 @@
+export const SPACING = {
+  EXTRA_SMALL: 4,
+  SMALL: 8,
+  MEDIUM: 16,
+  LARGE: 24,
+  EXTRA_LARGE: 32,
+};
+
+export const COLOR = {
+  WHITE: '#fff',
+  BLACK: '#000',
+  RED: '#ff0000',
+  BACKGROUND: '#fff',
+};
+
+export const FONT_SIZE = {
+  SMALL: 16,
+  MEDIUM: 18,
+  LARGE: 20,
+  EXTRA_LARGE: 32,
+};
+
+export const FONT = {
+  BLACK: 'Chivo-Black',
+  BLACK_ITALIC: 'Chivo-BlackItalic',
+  BOLD: 'Chivo-Bold',
+  BOLD_ITALIC: 'Chivo-BoldItalic',
+  ITALIC: 'Chivo-Italic',
+  LIGHT: 'Chivo-Light',
+  LIGHT_ITALIC: 'Chivo-LightItalic',
+  REGULAR: 'Chivo-Regular',
+};
+
+export const DEBUG = {
+  borderWidth: 1,
+  borderColor: COLOR.RED,
+};
