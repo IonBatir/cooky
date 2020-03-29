b40a56afbb9530401c78d0de47a32ddde6594afc
Ion Batîr <ionbatir@gmail.com>
2020-03-29 12:35:46 +0300

    Add main auth api calls

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,3 +2,11 @@ import auth from '@react-native-firebase/auth';
 
 export const onAuthStateChanged = handleAuthStateChanged =>
   auth().onAuthStateChanged(handleAuthStateChanged);
+
+export const login = (email, password) =>
+  auth().signInWithEmailAndPassword(email, password);
+
+export const register = (email, password) =>
+  auth().createUserWithEmailAndPassword(email, password);
+
+export const resetPassword = email => auth().sendPasswordResetEmail(email);
