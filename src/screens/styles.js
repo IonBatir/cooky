2948c91c46c66c9ef9fa537259ef1861258aca96
Ion Batîr <ionbatir@gmail.com>
2020-05-06 20:57:35 +0300

    Add text style to commonStyles

--- src/screens/styles.js
+++ src/screens/styles.js
@@ -12,6 +12,7 @@ export default StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: COLOR.BACKGROUND,
   },
   title: {
     fontFamily: FONT_FAMILY.BOLD,
@@ -24,5 +25,9 @@ export default StyleSheet.create({
     color: '#1D2226',
     opacity: 0.6,
     marginBottom: SPACING.EXTRA_LARGE,
+  },
+  text: {
+    fontFamily: FONT_FAMILY.LIGHT_ITALIC,
+    fontSize: FONT_SIZE.LARGE,
   },
 });
