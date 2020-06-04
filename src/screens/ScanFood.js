d22df456fddd2971889f4dd27415cea3b7a3fcd9
Ion Batîr <ionbatir@gmail.com>
2020-06-04 12:38:17 +0300

    Add ScanFood screen

---
+++ src/screens/ScanFood.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import commonStyles from './styles';
+
+export default function ScanFood() {
+  return (
+    <View style={commonStyles.centerContainer}>
+      <Text>Scan Food</Text>
+    </View>
+  );
+}
--- src/screens/index.js
+++ src/screens/index.js
@@ -4,3 +4,4 @@ export { default as ResetPassword } from './ResetPassword';
 export { default as Dashboard } from './Dashboard';
 export { default as FoodList } from './FoodList';
 export { default as AddFood } from './AddFood';
+export { default as ScanFood } from './ScanFood';
