6ec63a61f48f5f5c807fe9cc8413e6ad13a00641
Ion Batîr <ionbatir@gmail.com>
2020-08-22 21:47:07 +0300

    Addmn Recipe screen to navigation stack

--- src/navigation/RecipeStack.js
+++ src/navigation/RecipeStack.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { RecipeList } from '../screens';
-import { RECIPE_LIST_SCREEN } from '../constants';
+import { RecipeList, Recipe } from '../screens';
+import { RECIPE_LIST_SCREEN, RECIPE_SCREEN } from '../constants';
 
 const RecipeStack = createStackNavigator();
 
@@ -13,6 +13,7 @@ export default function() {
         component={RecipeList}
         options={{ headerShown: false }}
       />
+      <RecipeStack.Screen name={RECIPE_SCREEN} component={Recipe} />
     </RecipeStack.Navigator>
   );
 }
