6cfc763cecd0289d2c3a191335e57291af6219ae
Ion Batîr <ionbatir@gmail.com>
2020-08-22 18:16:08 +0300

    Add recipe list to navigation

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { FoodStack } from './';
-import { FOOD_STACK } from '../constants';
+import { FOOD_STACK, RECIPE_LIST_SCREEN } from '../constants';
+import { RecipeList } from '../screens';
 
-const AppNavigator = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function() {
   return (
-    <AppNavigator.Navigator>
-      <AppNavigator.Screen
+    <Tab.Navigator>
+      <Tab.Screen
         name={FOOD_STACK}
         component={FoodStack}
-        options={{ tabBarVisible: false }}
+        options={{
+          tabBarLabel: 'Food',
+          tabBarIcon: () => <Icon name="list" size={30} />,
+        }}
       />
-    </AppNavigator.Navigator>
+      <Tab.Screen
+        name={RECIPE_LIST_SCREEN}
+        component={RecipeList}
+        options={{
+          tabBarLabel: 'Recipe',
+          tabBarIcon: () => <Icon name="library-books" size={30} />,
+        }}
+      />
+    </Tab.Navigator>
   );
 }
