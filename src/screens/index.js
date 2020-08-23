805ce168373718923300afe1a48041e6412096ef
Ion Batîr <ionbatir@gmail.com>
2020-08-23 09:49:21 +0300

    Add AddRecipe screen

--- src/screens/index.js
+++ src/screens/index.js
@@ -7,4 +7,5 @@ export { default as AddFood } from './AddFood';
 export { default as ScanFood } from './ScanFood';
 export { default as RecipeList } from './RecipeList';
 export { default as Recipe } from './Recipe';
+export { default as AddRecipe } from './AddRecipe';
 export { default as Cook } from './Cook';
