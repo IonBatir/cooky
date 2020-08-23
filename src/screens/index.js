95e05b864d49767436a2d81bd97e903bb132b1f5
Ion Batîr <ionbatir@gmail.com>
2020-08-23 20:15:29 +0300

    Remove dashboard

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,6 @@
 export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as ResetPassword } from './ResetPassword';
-export { default as Dashboard } from './Dashboard';
 export { default as FoodList } from './FoodList';
 export { default as AddFood } from './AddFood';
 export { default as ScanFood } from './ScanFood';
